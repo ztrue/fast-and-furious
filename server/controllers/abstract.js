ea68ffa4cdb9e8cb99bbb3fd3c869c9f7ed961d6
Anton Grinin <gztrue@gmail.com>
2013-09-10 00:56:41 +0400

    Controllers

---
+++ server/controllers/abstract.js
@@ -0,0 +1,133 @@
+var eventEmitter = new (require('events').EventEmitter)();
+var _ = require('underscore');
+
+module.exports = {
+  /**
+   * Sockets
+   * @type {Object}
+   */
+  sockets: null,
+
+  /**
+   * Client emitters getter
+   * @type {Object.<string, function(Object=, Object=)>}
+   */
+  client: {},
+
+  /**
+   * Server emitters getter
+   * @type {Object.<string, function(Object=)>}
+   */
+  server: {},
+
+  /**
+   * Client emitters declaration
+   * @type {Object.<string, function()>}
+   */
+  clientEmitters: {},
+
+  /**
+   * Client listeners declaration
+   * @type {Object.<string, function(Object, Object=, function(?string=, Object=))>}
+   */
+  clientListeners: {},
+
+  /**
+   * Server emitters declaration
+   * @type {Object.<string, function()>}
+   */
+  serverEmitters: {},
+
+  /**
+   * Server listeners declaration
+   * @type {Object.<string, function(Object=)>}
+   */
+  serverListeners: {},
+
+  /**
+   * Controller bootstrap
+   * @param {Object} sockets Sockets
+   */
+  bootstrap: function(sockets) {
+    this.sockets = sockets;
+    this.initClientEmitters();
+    this.sockets.on('connection', this.initClientListeners.bind(this));
+    this.initServerEmitters();
+    this.initServerListeners();
+    // TODO custom onConnection
+    this.init();
+  },
+
+  /**
+   * Init client events emitters
+   */
+  initClientEmitters: function() {
+    // TODO use fn
+    _(this.clientEmitters).each(function(fn, event) {
+      this.client[event] = this.clientEmit.bind(this, event);
+    }.bind(this));
+  },
+
+  /**
+   * Init client events listeners
+   */
+  initClientListeners: function(socket) {
+    _(this.clientListeners).each(function(listener, event) {
+      socket.on(event, function(data, callback) {
+        listener.call(this, socket, data, function(err, data) {
+          data = data || {};
+          data.error = err || null;
+          data.success = !err;
+          callback.call(this, data);
+        }.bind(this));
+      }.bind(this));
+    }.bind(this));
+  },
+
+  /**
+   * Init server events emitters
+   */
+  initServerEmitters: function() {
+    // TODO use fn
+    _(this.serverEmitters).each(function(fn, event) {
+      this.server[event] = this.serverEmit.bind(this, event);
+    }.bind(this));
+  },
+
+  /**
+   * Init server events listeners
+   */
+  initServerListeners: function() {
+    _(this.serverListeners).each(function(listener, event) {
+      eventEmitter.on(event, function() {
+        listener.apply(this, arguments);
+      }.bind(this));
+    }.bind(this));
+  },
+
+  /**
+   * Emit event to client
+   * @param {string} event Event name
+   * @param {Object=} opt_emitter Emitter (sockets|socket|socket.broadcast)
+   * @param {Object=} opt_data Event data
+   */
+  clientEmit: function(event, opt_emitter, opt_data) {
+    // set default emitter (to all clients) if not passed
+    opt_emitter = opt_emitter || this.sockets;
+    opt_emitter.emit(event, opt_data);
+  },
+
+  /**
+   * Emit event to server
+   * @param {string} event Event name
+   * @param {...*} var_args Event data
+   */
+  serverEmit: function(event, var_args) {
+    eventEmitter.emit.apply(eventEmitter, arguments);
+  },
+
+  /**
+   * Controller init (at the end of the bootstrap)
+   */
+  init: function() {}
+};
