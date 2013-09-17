775c2ceddecbaad40a21203ef97a0eafd51ee89f
Anton Grinin <gztrue@gmail.com>
2013-09-17 23:52:52 +0400

    Abstract model

--- server/models/abstract.js
+++ server/models/abstract.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+  /**
+   * Constructor
+   * @param {Object=} opt_data Model data
+   * @constructor
+   */
+  constructor: function(opt_data) {},
+
+  /**
+   * Create new model
+   * @param {Object=} opt_data Model data
+   * @returns {Object}
+   */
+  new: function(opt_data) {
+    return new this.constructor(opt_data);
+  }
+};
+
+module.exports.constructor.prototype = module.exports;
